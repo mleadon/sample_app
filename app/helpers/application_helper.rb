dcc7b092560d86b5d39a81833ae26909abcbd69e
Michael Leadon <mleadon@cirm.ca.gov>
2011-01-25 17:03:55 -0800

    Finished Chapter 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,5 +9,8 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+  def cirmLogo
+    return image_tag("hoodie.jpg", :alt => "Sample App", :class => "round", :width => "100") 
+  end
 end
 
